694453873a7d5bbf501fe5c5d2e945a9db532022
Valentin Calomino <vcalomino@fi.uba.ar>
2023-10-01 19:36:03 -0300

    part1.13

--- part-1/anecdotes/src/App.jsx
+++ part-1/anecdotes/src/App.jsx
@@ -13,10 +13,29 @@ const App = () => {
   ];
 
   const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState({
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+    8: 0,
+  });
+
+  const voteAnecdote = () => {
+    const copy = { ...points };
+    copy[selected] += 1;
+    setPoints(copy);
+  };
 
   return (
     <div>
       {anecdotes[selected]}
+      <br />
+      <p>has {points[selected]} votes</p>
       <br />
       <button
         onClick={() =>
@@ -25,6 +44,7 @@ const App = () => {
       >
         next anecdote
       </button>
+      <button onClick={voteAnecdote}>vote</button>
     </div>
   );
 };
