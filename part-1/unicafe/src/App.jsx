89a464d43ec892323e4fc4619f974d4c8c8c4d0f
Valentin Calomino <vcalomino@fi.uba.ar>
2023-10-01 18:46:10 -0300

    part1.7

--- part-1/unicafe/src/App.jsx
+++ part-1/unicafe/src/App.jsx
@@ -5,7 +5,13 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const all = bad + neutral + good;
 
+  // Calculate average score
+  const average = all > 0 ? (good - bad) / all : 0;
+
+  // Calculate percentage of positive feedback
+  const positivePercentage = all > 0 ? (good / all) * 100 : 0;
   return (
     <div>
       <h2>give feedback</h2>
@@ -16,6 +22,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positivePercentage}%</p>
     </div>
   );
 };
