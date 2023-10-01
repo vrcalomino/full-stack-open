36bb696ab4d399d624d5811af1105ab9c5c31881
Valentin Calomino <vcalomino@fi.uba.ar>
2023-10-01 18:28:54 -0300

    part1.4

--- part-1/courseinfo/src/components/Content.jsx
+++ part-1/courseinfo/src/components/Content.jsx
@@ -4,16 +4,16 @@ const Content = (props) => {
   return (
     <div>
       <Part
-        part={props.part1}
-        exercises={props.exercises1}
+        part={props.parts[0].name}
+        exercises={props.parts[0].exercises}
       />
       <Part
-        part={props.part2}
-        exercises={props.exercises2}
+        part={props.parts[1].name}
+        exercises={props.parts[1].exercises}
       />
       <Part
-        part={props.part3}
-        exercises={props.exercises3}
+        part={props.parts[2].name}
+        exercises={props.parts[2].exercises}
       />
     </div>
   );
