4a3c84915b0d3996f4753b32be245aa8ee243c3e
Valentin Calomino <vcalomino@fi.uba.ar>
2023-10-01 19:54:34 -0300

    part2.1

---
+++ part-2/courseinfo2/src/components/Course.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header";
+import Content from "./Content";
+import Total from "./Total";
+
+const Course = (props) => {
+  return (
+    <div>
+      <Header course={props.course.name} />
+      <Content parts={props.course.parts} />
+      <Total parts={props.course.parts} />
+    </div>
+  );
+};
+
+export default Course;
