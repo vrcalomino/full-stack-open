ef1778f00fbe2513d0ee0fc39258c306e0d5c02e
Valentin Calomino <vcalomino@fi.uba.ar>
2023-10-02 21:17:53 -0300

    part2.4 && part2.5

--- part-2/courseinfo2/src/components/Content.jsx
+++ part-2/courseinfo2/src/components/Content.jsx
@@ -1,22 +1,15 @@
 import Part from "./Part";
 
 const Content = (props) => {
-  return (
-    <div>
+  return props.parts.map((part) => {
+    return (
       <Part
-        part={props.parts[0].name}
-        exercises={props.parts[0].exercises}
+        key={part.id}
+        part={part.name}
+        exercises={part.exercises}
       />
-      <Part
-        part={props.parts[1].name}
-        exercises={props.parts[1].exercises}
-      />
-      <Part
-        part={props.parts[2].name}
-        exercises={props.parts[2].exercises}
-      />
-    </div>
-  );
+    );
+  });
 };
 
 export default Content;
