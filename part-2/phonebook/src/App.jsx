9368834a990372fcbfa1f01e922137e0117cd589
vrcalomino <vrcalomino@gmail.com>
2023-10-02 21:32:59 -0300

    part2.6

---
+++ part-2/phonebook/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newPerson = { name: newName };
+    const copy = [...persons];
+    setPersons(copy.concat(newPerson));
+    setNewName("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name:{" "}
+          <input
+            value={newName}
+            onChange={(e) => setNewName(e.target.value)}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons !== null
+        ? persons.map((person) => <p key={person.name}>{person.name}</p>)
+        : null}
+    </div>
+  );
+};
+
+export default App;
