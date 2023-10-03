de6f8bf678cd5ff20203297afbd58c99e85e5822
vrcalomino <vrcalomino@gmail.com>
2023-10-02 21:42:52 -0300

    part2.8

--- part-2/phonebook/src/App.jsx
+++ part-2/phonebook/src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "030-12312" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -10,12 +13,14 @@ const App = () => {
     if (found) {
       alert(`${newName} is already in the phonebook`);
       setNewName("");
+      setNewNumber("");
       return;
     }
-    const newPerson = { name: newName };
+    const newPerson = { name: newName, number: newNumber };
     const copy = [...persons];
     setPersons(copy.concat(newPerson));
     setNewName("");
+    setNewNumber("");
   };
 
   return (
@@ -28,6 +33,13 @@ const App = () => {
             value={newName}
             onChange={(e) => setNewName(e.target.value)}
           />
+          <div>
+            number:{" "}
+            <input
+              value={newNumber}
+              onChange={(e) => setNewNumber(e.target.value)}
+            />
+          </div>
         </div>
         <div>
           <button type="submit">add</button>
@@ -35,7 +47,11 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {persons !== null
-        ? persons.map((person) => <p key={person.name}>{person.name}</p>)
+        ? persons.map((person) => (
+            <p key={person.name}>
+              {person.name} {person.number}
+            </p>
+          ))
         : null}
     </div>
   );
